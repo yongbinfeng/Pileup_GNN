3e65b5ba3ceea6eb536f88bf557919ec90d7f4d7
Tianchun Li <73758638+boilerchun@users.noreply.github.com>
2021-10-11 16:35:04 -0400

    Update train_fastsim_sup.py

--- fast_simulation/train_fastsim_sup.py
+++ fast_simulation/train_fastsim_sup.py
@@ -36,9 +36,9 @@ def arg_parse():
                         help='pileup level for the dataset')
     parser.add_argument('--deltar', type=float,
                         help='deltaR for connecting particles when building the graph')
-    parser.add_argument('--training_path', type=str,
+    parser.add_argument('--training_path', type=str, required=True,
                         help='path for the training graphs')
-    parser.add_argument('--validation_path', type=str,
+    parser.add_argument('--validation_path', type=str, required=True,
                         help='path for the validation graphs')
     parser.add_argument('--save_dir', type=str, required=True,
                         help='directory to save the trained model and plots')
